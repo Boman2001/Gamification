111b84b4c67829d1db730af9d156c16a3f14b57c
m-don <mljj.donkersloot@student.avans.nl>
2021-06-08 18:41:55 +0200

    Avatar muziek keuze

---
+++ Assets/Scripts/Avatar/MusicScreen/UiInventory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Databases;
+using Domain;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Avatar.MusicScreen
+{
+    public class UiInventory : MonoBehaviour
+    {
+        public GameObject slotPrefab;
+        public MusicDatabase database;
+        public GameObject listPost;
+        private void Awake()
+        {
+            
+            GameObject newObj; // Create GameObject instance
+
+            for (int i = 0; i < database.songs.Count; i++)
+            {
+                // Create new instances of our prefab until we've created as many as we specified
+                newObj = (GameObject)Instantiate(slotPrefab, transform);
+                UiItem item = newObj.GetComponentInChildren<UiItem>();
+                item.song = database.FindSongById(i);
+                item.text.text = item.song.title;
+                item.GetComponent<Button>().onClick.AddListener(() =>
+                {
+                    TaskOnClick(item);
+                });
+            }
+        }
+        
+        void TaskOnClick(UiItem song){    
+            UiItem post = Instantiate(song, listPost.transform);
+        }
+    }
+}
---
+++ Assets/Scripts/Avatar/MusicScreen/UiItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Domain;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Avatar.MusicScreen
+{
+    public class UiItem : MonoBehaviour
+    {
+        public Song song;
+        public TMP_Text text;
+
+        private void Awake()
+        {
+            text = GetComponentInChildren<TMP_Text>();
+        }
+        
+        
+        
+    }
+}
