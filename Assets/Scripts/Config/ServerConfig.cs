464ecf4650eda099b44e89d580500d7afe3ecd95
MAQUES <m.bogaers@student.avanms.nl>
2021-06-09 19:59:27 +0200

    added backgrounds and fixed home and login

--- Assets/Scripts/Config/ServerConfig.cs
+++ Assets/Scripts/Config/ServerConfig.cs
@@ -2,6 +2,6 @@
 {
     public class ServerConfig
     {
-        public const string API_URL = "http://localhost:5000/api/v1";
+        public const string API_URL = "http://theater-gamification.azurewebsites.net/api/v1";
     }
 }
